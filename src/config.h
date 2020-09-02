ac78bbd281b424fe06a77f51857d948bc11561bf
unknown <preben.ulltang@gmail.com>
2020-09-02 20:43:36 +0200

    first commit

---
+++ src/config.h
@@ -0,0 +1,15 @@
+#define SECRET_SSID "4G-Mobile-WiFi-E509"
+#define SECRET_PASS "bb89eigm"
+#define MQTT_ADDRESS "192.168.8.105"
+#define MQTT_USERNAME "espmqtt"
+#define MQTT_PASS "lego1337"
+int mqttPort = 1883;
+
+
+#define ROTARY_PIN1  18
+#define ROTARY_PIN2 19
+#define BUTTON_PIN  23
+#define CLICKS_PER_STEP   4   // this number depends on your rotary encoder 
+
+#define SDA 21
+#define SDL 22
